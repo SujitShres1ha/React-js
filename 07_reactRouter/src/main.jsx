be39bb5e6ae122d71ea70206346cef2c48fc05c9
SujitShres1ha <sthasujit2062@gmail.com>
2024-05-19 13:11:57 +0545

    Add Layout component and update routing

--- 07_reactRouter/src/main.jsx
+++ 07_reactRouter/src/main.jsx
@@ -2,9 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import About from './components/About.jsx'
+import Home from './components/Home.jsx'
+import Contact from './components/Contact.jsx'
 
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home/>
+//       },
+//       {
+//         path: 'about',
+//         element: <About/>
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact/>
+//       }
+//     ]
+//   }
+// ]
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = '/' element = {<Layout/>}>
+      <Route path = '' element = {<Home/>}/>
+      <Route path = 'about' element = {<About/>}/>
+      <Route path = 'contact' element = {<Contact/>}/>
+    </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- README.md
+++ README.md
@@ -3,4 +3,10 @@ I'm keeping all my notes here starting with 07_reactRouter.
 //07_reactRouter
 react-router-dom is a third-party library, not in core react
 
+Link is a substitue for <a> tag as <a> reloads the page
+Navlink is a special type of Link which can be "active" or "inactive" provided its "to" prop matches the current url. We can style the navlink when it is active. ("to" is a substitue for "href")
 
+`Layout` is a way to keep the same look on different pages. For example, if you have a header and footer that you want on every page, you can put them in your `Layout`. Then, you just change what's between them for different pages through `Outlet`.
+`Outlet` like a placeholder for your pages. When you navigate to a different page, `Outlet` gets replaced with the content of that page.
+
+We define routing paths in a variable and pass it to `RouterProvider` as a prop.
