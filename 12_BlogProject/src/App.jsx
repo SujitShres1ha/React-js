3fd7076b8e90cab9a69ff7fa0ceb70e32f8b6221
SujitShres1ha <sthasujit2062@gmail.com>
2024-06-07 20:05:13 +0545

    Add files and configurations for BlogProject

---
+++ 12_BlogProject/src/App.jsx
@@ -0,0 +1,15 @@
+import conf from './config/config'
+
+import './App.css'
+
+function App() {
+  console.log(conf.appwriteUrl)
+
+  return (
+    <>
+     <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
